4a46e2338468345bf27407aca7d872f580139260
Bobirechek <kostylevtimofej354@gmail.com>
2026-03-20 19:32:20 +0300

    hhh

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -3,10 +3,10 @@ import ListM from "./ListM";
 import { useState } from "react";
 
 export default function App () {
-    const [notes, setNotes] = useState([{id:0, head: "QQWE", body: "text"}, {id:1, head: "QWEQWE", body: "text"}]);
+    const [notes, setNotes] = useState([{id:0, head: "Header", body: "text"}]);
 
     return <div className="main" style={ style }>
-        <h1 className="header">mashakakashkaitimatozhe</h1>
+        <h1 className="header">ToDo list</h1>
         <ListM arr={ notes } setArr={ setNotes } />
     </div>;
 }
